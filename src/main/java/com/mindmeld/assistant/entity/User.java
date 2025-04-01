3293718b6fe2ff1ecd7e2fd585468c7f1f37b40b
ashwinyadav09 <ydashwin09@gmail.com>
2025-04-01 16:29:55 +0530

    Singup and Login Error solved

--- src/main/java/com/mindmeld/assistant/entity/User.java
+++ src/main/java/com/mindmeld/assistant/entity/User.java
@@ -2,13 +2,17 @@ package com.mindmeld.assistant.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
+@Table(name = "user")
 @Data
 public class User {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
